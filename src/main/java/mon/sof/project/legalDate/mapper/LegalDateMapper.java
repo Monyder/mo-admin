0865398cfedcfd75136134fbf250e59edaed2926
Monyder <mon418@163.com>
2020-07-16 16:36:08 +0800

    添加学习资料

---
+++ src/main/java/mon/sof/project/legalDate/mapper/LegalDateMapper.java
@@ -0,0 +1,16 @@
+package mon.sof.project.legalDate.mapper;
+
+import mon.sof.project.legalDate.entity.LegalDate;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import mon.sof.common.orm.IBaseAction;
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author zxm
+ * @since 2020-03-17
+ */
+public interface LegalDateMapper extends BaseMapper<LegalDate>,IBaseAction<LegalDate> {
+
+}
---
+++ src/main/java/mon/sof/project/legalDate/service/LegalDateService.java
@@ -0,0 +1,28 @@
+package mon.sof.project.legalDate.service;
+
+import mon.sof.project.legalDate.entity.LegalDate;
+import mon.sof.project.legalDate.mapper.LegalDateMapper;
+import mon.sof.common.orm.IBaseAction;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import mon.sof.common.orm.AbstractBaseAction;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author zxm
+ * @since 2020-03-17
+ */
+@Service
+public class LegalDateService extends AbstractBaseAction<LegalDateMapper, LegalDate> {
+
+    @Autowired
+    LegalDateMapper LegalDateMappers;
+
+    @Override
+    public IBaseAction<LegalDate> getMapper() {
+        return LegalDateMappers;
+    }
+}
